664dd59babe0baded24362b87b9554a0fac01584
barqi <barqi.fuady@windowslive.com>
2022-03-27 22:47:06 +0700

    first init

---
+++ components/Songs.tsx
@@ -0,0 +1,21 @@
+import { useRecoilValue } from "recoil"
+import { playlistState } from "../atoms/playlistAtom"
+import Song from "./Song"
+
+const Songs = () => {
+
+    const playlist:any = useRecoilValue(playlistState)
+
+    return(
+        <div className="px-8 flex flex-col space-y-1 pb-28 text-white">
+            {
+            
+            playlist?.tracks.items.map((track,i) => (
+                <Song key={track.track.id} track={track} order={i} />
+            ))
+            }
+        </div>
+    )
+}
+
+export default Songs
---
+++ hooks/useSongInfo.ts
@@ -0,0 +1,32 @@
+import useSpotify from "./useSpotify"
+import {currentTrackIdState} from '../atoms/songAtom'
+import { useRecoilState } from 'recoil'
+import { useEffect, useState } from "react"
+
+const useSongInfo = () => {
+    const spotifyApi = useSpotify()
+    const [currentTrackId,setCurrentTrackId] = useRecoilState(currentTrackIdState)
+    const [songInfo,setSongInfo] = useState(null)
+
+    useEffect(() => {
+        
+        const fetchSongInfo = async () => {
+            if(!currentTrackId) return
+
+            const trackInfo = await fetch(`https://api.spotify.com/v1/tracks/${currentTrackId}`,{
+                headers:{
+                    Authorization:`Bearer ${spotifyApi.getAccessToken()}`
+                }
+            }).then(res => res.json())
+
+            setSongInfo(trackInfo)
+        }
+
+        fetchSongInfo()
+
+    },[currentTrackId,spotifyApi])
+
+    return songInfo
+} 
+
+export default useSongInfo
